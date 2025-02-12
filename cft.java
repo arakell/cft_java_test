2fcf2e3f89e2e8b889e8a7df57804eb1b15503d8
Arina <arina.safronova@inbox.ru>
2025-02-13 01:19:48 +0700

    Добавлен ввод параметров

--- cft.java
+++ cft.java
@@ -1,7 +1,59 @@
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class cft {
+
+
+    static void err(String err){
+        System.out.println(err);
+        System.exit(0);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        boolean isO;
+        boolean isP;
+        boolean isA;
+        boolean isS;
+        boolean isF;
+
+        /* Чтение опций */
+        for(int i = 0; i < args.length; i++){
+
+            switch(args[i]){
+
+                case "-o":
+                    isO = true;
+                    i++;
+                    if(i >= args.length){
+                        err("Не передан аргумент для параметра -o");
+                    }
+                    //TODO обернуть в exception если путь некорректный
+                    Path path = Paths.get(args[i]);
+
+                    break;
+                case "-p":
+                    isP = true;
+
+                    /* чтение префикса */
+
+                    break;
+                case "-a":
+                    isA = true;
+                    break;
+                case "-s":
+                    isS = true;
+                    break;
+                case "-f":
+                    isF = true;
+                    break;
+
+            }
+
+
+
+        }
+
     }
 }
