c2d5e3f1dc506a8dc4f88245c8ade5f0473b7807
Arina <arina.safronova@inbox.ru>
2025-02-16 22:37:55 +0700

    Кажется final

--- cft.java
+++ cft.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,8 +11,38 @@ import java.util.List;
 public class cft {
 
 
+    static Path path = Paths.get("");
+    static String pref = "";
+    /* Переменные чтобы при ошибке при удалении созданных файлов не стереть те, что уже были */
+    static boolean isDirAlreadeExsts = false;
+    static boolean isIntExsts = false;
+    static boolean isFloatExsts = false;
+    static boolean isStringExsts = false;
+    /* Костыль для обработки пути */
+    static String slash = "";
+
     static void err(String err){
         System.out.println(err);
+        try{
+            /* Удаляем созданные файлы, если они не существовали ранее */
+            if(!isIntExsts){
+                Files.delete(Paths.get(path.toString() + slash + pref + "integers.txt"));
+            }
+            if(!isFloatExsts){
+                Files.delete(Paths.get(path.toString() + slash + pref + "floats.txt"));
+            }
+            if(!isStringExsts){
+                Files.delete(Paths.get(path.toString() + slash + pref + "strings.txt"));
+            }
+            if(!isDirAlreadeExsts){
+                /* Удаляет только последний по вложенности каталог что не очень правильно */
+                Files.delete(Paths.get(path.toString()));
+            }
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    
         System.exit(0);
     }
 
@@ -25,8 +54,7 @@ public class cft {
         boolean isA = false;
         boolean isS = false;
         boolean isF = false;
-        /* Путь для сохранения результатов */
-        Path path = Paths.get("");
+        
         /* Массив файлов для чтения */
         List<Path> filePaths = new ArrayList<>();
         /* Названия файлов */
@@ -37,10 +65,10 @@ public class cft {
         int countInt = 0;
         int countFloat = 0;
         int countString = 0;
-
-        int minInt = 0;
-        int maxInt = 0;
-        int sumInt = 0;
+        /* Переменные для статистики */
+        long minInt = 0;
+        long maxInt = 0;
+        long sumInt = 0;
         float minFloat = 0;
         float maxFloat = 0;
         float sumFloat = 0;
@@ -55,6 +83,7 @@ public class cft {
                 case "-o":
 
                     isO = true;
+                    slash = "/";
                     i++;
                     if(i >= args.length){
 
@@ -63,20 +92,21 @@ public class cft {
                     }
                     try{
 
-                        /*  TODO
+                        /*
                          * Не нравится это решение, но я не знаю почему каталог не создается 
-                         * если первый символ \, хотя внутри конструктора есть нормализация
+                         * если первый символ /, хотя внутри конструктора есть нормализация
                          */
                         if (args[i].startsWith("/")) {
                             args[i] = args[i].substring(1);
                         }
                         File theDir = new File(args[i]);
+                        path = Paths.get(args[i]);
+                        if(theDir.exists()) isDirAlreadeExsts = true;
                         if (!theDir.exists() && !theDir.mkdirs()){
 
                             err("Не удалось создать каталог, убедитесь в правильности названия, пример /test/case");
 
                         };
-                        path = Paths.get(args[i]);
 
                     } catch(Exception e){
 
@@ -99,7 +129,7 @@ public class cft {
                     nameIntFile = args[i] + nameIntFile;
                     nameFloatFile = args[i] + nameFloatFile;
                     nameStringFile = args[i] + nameStringFile;
-
+                    pref = args[i];
                     break;
 
                 case "-a":
@@ -125,7 +155,7 @@ public class cft {
 
                     } catch(Exception e){
 
-                        err("Произошла ошибка при попытке считать имя файла, проверьте правильность введенных данных");
+                        err("Произошла ошибка при попытке считать имя файла для чтения, проверьте правильность введенных данных");
 
                     }
                     break;
@@ -139,24 +169,30 @@ public class cft {
         Path resString = null;
         try{
 
-            resInt = Paths.get(path + "/" + nameIntFile);
-            resFloat = Paths.get(path + "/" + nameFloatFile);
-            resString = Paths.get(path + "/" + nameStringFile);
+            resInt = Paths.get(path + slash + nameIntFile);
+            resFloat = Paths.get(path + slash + nameFloatFile);
+            resString = Paths.get(path + slash + nameStringFile);
 
             /* Если выбрана опция -a, но каких-то файлов нет - создаем их */
             if(isA){
-
+                System.out.println("here");
                 File f = new File (resInt.toString());
                 if(!f.exists()){
                     Files.createFile(resInt);
+                } else{
+                    isIntExsts = true;
                 }
                 f = new File (resFloat.toString());
                 if(!f.exists()){
                     Files.createFile(resFloat);
+                } else{
+                    isFloatExsts = true;
                 }
                 f = new File (resString.toString());
                 if(!f.exists()){
                     Files.createFile(resString);
+                } else{
+                    isStringExsts = true;
                 }
 
             } else{
@@ -196,14 +232,13 @@ public class cft {
                 BufferedWriter writerString = Files.newBufferedWriter(resString, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             ){
 
-                
                 String line;
                 Float fl;
-                Integer integer;
+                Long integer;
                 String str;
                 while ((line = reader.readLine()) != null) {
                     try{
-                        integer = Integer.parseInt(line);
+                        integer = Long.parseLong(line);
                         writerInt.write(integer.toString());
                         writerInt.newLine();
                         countInt++;
@@ -274,7 +309,42 @@ public class cft {
                 }
             } catch (Exception e) {
 
-                err("Файл не существует, либо введено неверное имя");
+                err("Файл для чтения не существует, либо введено неверное имя");
+
+            }
+
+        }
+
+        /* Удаление лишних файлов */
+        if (!isIntExsts && countInt == 0){
+
+            try{
+            Files.delete(resInt);
+            } catch(Exception e){
+
+                System.out.println("Создан лишний файл для целых чисел");
+
+            }
+
+        }
+        if (!isFloatExsts && countFloat == 0){
+
+            try{
+            Files.delete(resFloat);
+            } catch(Exception e){
+
+                System.out.println("Создан лишний файл для вещественных чисел");
+
+            }
+
+        }
+        if (!isStringExsts && countString == 0){
+
+            try{
+            Files.delete(resString);
+            } catch(Exception e){
+
+                System.out.println("Создан лишний файл для строк");
 
             }
 
@@ -289,7 +359,7 @@ public class cft {
         }
 
         if(isF){
-
+            
             System.out.println("Максимальное среди целых чисел: " + maxInt);
             System.out.println("Минимальное среди целых чисел: " + minInt);
             System.out.println("Сумма целых чисел: " + sumInt);
